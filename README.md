67baad8b43a1241a60b393a77506da4e2a3adf14
Ramon Liao <ramonliao.yc@gmail.com>
2023-05-15 10:05:58 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Todo List App
+
+<img src="TodoList.gif" alt="Cover" style="display:block; margin-left: auto;
+margin-right: auto;width: 45%;"/>
+
+This is a Todo-List app programmed with HTML/CSS/JavaScript.
+
+1. User may open "index.html" to start an app.
+2. User may enter description, month, and date to create a new item on todo list.
+3. Completion button is toggle designed. User may click the button as the item is comepleted.
+4. Sorting function is also designed. User may click the button to re-order the whole items in ascendance by comparing months and dates.
+5. Deletion button can let user to remove an item from the list.
+6. The list will be stored in local storage (in computer). Everytime when user opens the website, the todo list will be shown on the scree.
+
+**_Start to organise your todo list and achieve a happy life!!!_**
---
+++ app.js
@@ -0,0 +1,203 @@
+// create a todo
+function createToDo(todoText, todoMonth, todoDate) {
+  let todo = document.createElement("div");
+  todo.classList.add("todo");
+  let text = document.createElement("p");
+  text.classList.add("todo-text");
+  text.innerText = todoText;
+  let time = document.createElement("p");
+  time.classList.add("todo-time");
+  time.innerText = todoMonth + "/" + todoDate;
+
+  todo.appendChild(text);
+  todo.appendChild(time);
+
+  return todo;
+}
+
+function createCompleteBtn() {
+  let completeButton = document.createElement("button");
+  completeButton.classList.add("complete");
+  completeButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+  completeButton.addEventListener("click", (e) => {
+    let todoItem = e.target.parentElement;
+    todoItem.classList.toggle("done");
+  });
+
+  return completeButton;
+}
+
+function createRubbishBtn() {
+  let rubbishButton = document.createElement("button");
+  rubbishButton.classList.add("rubbish");
+  rubbishButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+  rubbishButton.addEventListener("click", (e) => {
+    let todoItem = e.target.parentElement;
+
+    todoItem.addEventListener("animationend", () => {
+      //   console.log(todoItem.children[0]);
+      // remove from localStorage
+      let text = todoItem.children[0].innerText;
+      let myListArray = JSON.parse(localStorage.getItem("list"));
+      myListArray.forEach((item, index) => {
+        if (item.todoText == text) {
+          myListArray.splice(index, 1);
+          localStorage.setItem("list", JSON.stringify(myListArray));
+        }
+      });
+
+      todoItem.remove();
+    });
+    todoItem.style.animation = "scaleDown 0.3s forwards";
+  });
+
+  return rubbishButton;
+}
+
+let section = document.querySelector("section");
+let add = document.querySelector("form button");
+add.addEventListener("click", (e) => {
+  // prevent form from being submitted
+  e.preventDefault();
+
+  // get the input values
+  let form = e.target.parentElement;
+  let todoText = form.children[0].value.trim();
+  let todoMonth = form.children[1].value;
+  let todoDate = form.children[2].value;
+
+  // simple data validation
+  if (todoText === "") {
+    alert("Please Enter Some Text.");
+    return;
+  }
+
+  if (todoMonth < 1 || todoMonth > 12) {
+    alert("Please Enter A Valid Month.");
+    form.children[1].value = "";
+    return;
+  }
+
+  if (todoDate < 1 || todoDate > 31) {
+    alert("Please Enter A Valid Date.");
+    form.children[2].value = "";
+    return;
+  }
+
+  // create a todo
+  let todo = createToDo(todoText, todoMonth, todoDate);
+
+  // create green check and red rubbish bin
+  let completeButton = createCompleteBtn();
+  let rubbishButton = createRubbishBtn();
+
+  todo.appendChild(completeButton);
+  todo.appendChild(rubbishButton);
+
+  todo.style.animation = "scaleUp 0.3s forwards";
+
+  // create an object
+  let myTodo = {
+    todoText,
+    todoMonth,
+    todoDate,
+  };
+
+  // store data into an array of objects
+  let myList = localStorage.getItem("list");
+  if (myList == null) {
+    localStorage.setItem("list", JSON.stringify([myTodo]));
+  } else {
+    let myListArray = JSON.parse(myList);
+    myListArray.push(myTodo);
+    localStorage.setItem("list", JSON.stringify(myListArray));
+  }
+
+  console.log("Store data");
+  console.log(JSON.parse(localStorage.getItem("list")));
+
+  form.children[0].value = ""; // clear the text input
+  section.appendChild(todo);
+});
+
+// load data
+loadData();
+
+function loadData() {
+  let myList = localStorage.getItem("list");
+  if (myList !== null) {
+    let myListArray = JSON.parse(myList);
+    myListArray.forEach((item) => {
+      let todo = createToDo(item.todoText, item.todoMonth, item.todoDate);
+      let completeButton = createCompleteBtn();
+      let rubbishButton = createRubbishBtn();
+
+      todo.appendChild(completeButton);
+      todo.appendChild(rubbishButton);
+
+      section.appendChild(todo);
+    });
+  }
+}
+
+function mergeTime(arr1, arr2) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (Number(arr1[i].todoMonth) > Number(arr2[j].todoMonth)) {
+      result.push(arr2[j]);
+      j++;
+    } else if (Number(arr1[i].todoMonth) < Number(arr2[j].todoMonth)) {
+      result.push(arr1[i]);
+      i++;
+    } else if (Number(arr1[i].todoMonth) == Number(arr2[j].todoMonth)) {
+      if (Number(arr1[i].todoDate) > Number(arr2[j].todoDate)) {
+        result.push(arr2[j]);
+        j++;
+      } else {
+        result.push(arr1[i]);
+        i++;
+      }
+    }
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+
+  return result;
+}
+
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  } else {
+    let middle = Math.floor(arr.length / 2);
+    let right = arr.slice(0, middle);
+    let left = arr.slice(middle, arr.length);
+    return mergeTime(mergeSort(right), mergeSort(left));
+  }
+}
+
+let sortButton = document.querySelector("div.sort button");
+sortButton.addEventListener("click", () => {
+  // sort data
+  let sortedArray = mergeSort(JSON.parse(localStorage.getItem("list")));
+  localStorage.setItem("list", JSON.stringify(sortedArray));
+
+  // remove data from the screen
+  let len = section.children.length;
+  for (let i = 0; i < len; i++) {
+    section.children[0].remove();
+  }
+
+  // load data
+  loadData();
+});
